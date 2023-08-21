ec9568469e1552847a017b706f25282a616e3ae2
Julian Martinez <julian_martinez28@outlook.com>
2023-08-20 18:13:28 -0700

    Update index.tsx

--- components/organisms/hello-world/index.tsx
+++ components/organisms/hello-world/index.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent, useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { Card } from "../../atoms";
 import * as helloWorldContract from "hello-world-contract";
 import { useAccount } from "../../../hooks";
