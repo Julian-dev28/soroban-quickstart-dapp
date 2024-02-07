b70201d8f12f3f3f7946a3038a1e53a773d8a72d
Julian Martinez <julian_martinez28@outlook.com>
2024-02-07 14:48:51 -0800

    update

--- hello.sh
+++ hello.sh
@@ -2,7 +2,7 @@ soroban contract invoke \
 --network futurenet \
 --wasm target/wasm32-unknown-unknown/release/soroban_modified_hello_world_contract.wasm \
 --source SCTW7YZCPF5KLB6ALZ2RBUIG3FQIVEJYK5OFCBHZU5637L44AD5OJZYS \
---id CB27P5AAV5BCN22TKA2R2C32LTOSINAPOPAKU4FYLHBSUCR2LITQTQHF \
+--id CAF4R7W7U3JODI7CSFLSSUTSGG4H5EHGHTVIPNED7JA65FRM66AABQXS \
 -- \
 hello \
 --to "Bob"
